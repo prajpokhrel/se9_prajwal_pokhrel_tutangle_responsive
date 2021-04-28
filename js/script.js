10a31f79ced6bea33bf544a4ae3b79647ffe40b7
Prajwal Pokhrel <prajwal.pokhrel18@gmail.com>
2021-04-28 08:06:15 +0545

    updated navigation toggle in small size devices

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 // Import js if you add some code here
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggle-navigation');
+    const togglerActive = document.getElementById('toggle-navigation');
+    const navWrapper = document.getElementById('sub-navigation-wrapper-id');
+    const navContainer = document.getElementById('sub-navigation-container-id');
+    toggleButton.addEventListener("click", toggleNavigation);
+
+    function toggleNavigation() {
+        const toggledNavigation = document.getElementById('toggled-navigation');
+        if (toggledNavigation.style.display === "none") {
+            toggledNavigation.style.display = "block";
+            togglerActive.style.color = "#2fc1da";
+            navWrapper.style.height = "250px";
+            navContainer.style.height = "250px";
+
+        } else {
+            toggledNavigation.style.display = "none";
+            togglerActive.style.color = "#323232";
+            navWrapper.style.height = "100px";
+            navContainer.style.height = "100px";
+        }
+
+    }
+});
